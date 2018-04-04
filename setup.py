6fcb60e0eb59aa9d7f2707351b3845c5fb0ca978
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-04-03 22:09:38 -0400

    fixes up project for better testing

--- setup.py
+++ setup.py
@@ -8,27 +8,28 @@ setup(
     url="https://github.com/shawalli/flask-github-webhook",
     author="Shawn Wallis",
     author_email="shawn.p.wallis@gmail.com",
-    #   license='Apache 2.0',
+    license='Apache 2.0',
     py_modules=['flask_github_webhook'],
     package_dir={'': 'src'},
     install_requires=[
-        'Flask',
         'github-webhook',
+        'six'
     ],
-
-    #   classifiers=[
-    #       'Development Status :: 4 - Beta',
-    #       'Framework :: Flask',
-    #       'Environment :: Web Environment',
-    #       'Intended Audience :: Developers',
-    #       'Intended Audience :: System Administrators',
-    #       'License :: OSI Approved :: Apache Software License',
-    #       'Operating System :: MacOS :: MacOS X',
-    #       'Operating System :: Microsoft :: Windows',
-    #       'Operating System :: POSIX',
-    #       'Programming Language :: Python :: 2',
-    #       'Programming Language :: Python :: 3',
-    #       'Topic :: Software Development :: Version Control'
-    #       ],
-    #   test_suite='nose.collector'
+    tests_require=[
+        'flask',
+        'pytest'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Flask',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Version Control :: Git'
+    ]
 )

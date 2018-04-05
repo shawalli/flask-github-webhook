80137eaa0a4fc8bb4ad00e29dcae390aa249e8f4
Shawn Wallis <shawn.p.wallis@gmail.com>
2018-04-04 22:49:09 -0400

    adds tests to test extension initialization

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,17 @@ from flask_github_webhook import GithubWebhook
 
 
 class TestApp(object):
+    def test_direct_initialization(self, app):
+        webhook = GithubWebhook(app)
+
+        assert app.extensions['github_webhook'] == webhook
+
+    def test_extension_pattern_initialization(self, app):
+        webhook = GithubWebhook()
+        webhook.init_app(app)
+
+        assert app.extensions['github_webhook'] == webhook
+
     def test_default_config(self, app):
         webhook = GithubWebhook()
         webhook.init_app(app)
